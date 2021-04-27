1fad2a106ad4ba9c2130eba2114d1bc757ee8e2c
icetong001 <doralee1005@gmail.com>
2021-04-27 16:25:20 +0800

    BOXES + array

--- src/main/java/com/tong/test/Tester.java
+++ src/main/java/com/tong/test/Tester.java
@@ -2,13 +2,20 @@ package com.tong.test;
 
 public class Tester {
     public static void main(String[] args) {
-        Box[] boxes = {new Box1(), new Box2(),new Box3(),new Box4(),new Box5(),new Box6()};
-
+    //    Box[] boxes = {new Box1(), new Box2(),new Box3(),new Box4(),new Box5(),new Box6()};
+        Box[] boxes=new Box[5];
+        boxes[0]=new Box1();
+        boxes[1]=new Box2();
+        boxes[2]=new Box3();
+        boxes[3]=new Box4();
+        boxes[4]=new Box5();
+        System.out.println(boxes[0].price);
 
         int length = 15;
         int width = 15;
         int height = 15;
         for (Box box : boxes) {
+           // Box1 box1=(Box1) box;
             if (box.validate(length, width, height)) {
                 System.out.println(box.name + " it is!, " + box.price);
                 break;
